213b8fa53ab575bcb5a06b0ba8918f46c256f12f
nxgupta <neeraj.gupta6525@gmail.com>
2023-04-13 22:26:37 +0530

    deploying code

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://solarsquareschool.netlify.com'],
+  origin: ['http://localhost:5173', 'https://solarsquareschool.netlify.app'],
   credentials: true,
 };
 app.use(cors(corsOptions));
